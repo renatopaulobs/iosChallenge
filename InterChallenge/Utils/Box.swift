77a650e8601984463a6f07428ade7700b2b16d3c
Paulo Renato <renatopaulobs@gmail.com>
2021-10-09 11:31:47 -0300

    [main]: Initializing Architecture Refactoring To MVVM

---
+++ InterChallenge/Utils/Box.swift
@@ -0,0 +1,27 @@
+//
+//  Box.swift
+//  InterChallenge
+//
+//  Created by Paulo  on 09/10/21.
+//
+
+import Foundation
+
+final class Box<T> {
+    typealias Listener = (T) -> Void
+    var listener: Listener?
+    var value: T {
+        didSet {
+            listener?(value)
+        }
+    }
+    
+    init(_ value: T) {
+        self.value = value
+    }
+    
+    func bind(listener: Listener?) {
+        self.listener = listener
+        listener?(value)
+    }
+}
---
+++ InterChallenge/ViewModels/ChallengeViewModel.swift
@@ -0,0 +1,8 @@
+//
+//  ChallengeViewModel.swift
+//  InterChallenge
+//
+//  Created by Paulo  on 09/10/21.
+//
+
+import Foundation
